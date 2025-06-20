0dd8123d719de1892b66fa9030537a3528489ae1
saimanasamuppalaneni <saimanasamuppalaneni@gmail.com>
2025-06-20 11:52:47 +0530

    oddand even

---
+++ class 5 conditional statement/oddeven.py
@@ -0,0 +1,7 @@
+# odd or even
+num=int(input("enter the number:"))
+print("the number to be checked :",num)
+if num%2==0:
+    print("the number is an even number")
+else:
+    print("the number is an odd number")
