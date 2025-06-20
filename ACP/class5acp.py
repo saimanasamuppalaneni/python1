5ff3be4b91b5eefe2cf2c7d3c2596928089ed31c
saimanasamuppalaneni <saimanasamuppalaneni@gmail.com>
2025-06-20 12:13:58 +0530

    summer times

---
+++ ACP/class5acp.py
@@ -0,0 +1,6 @@
+# wearing cloaths
+temp=int(input("enter the temperature"))
+if temp<20:
+    print("the temperature is less, you should wear woolen cloths ")
+else:
+    print("the temperature is little high ,you should wear light and cotton cloths")
