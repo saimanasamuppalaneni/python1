3ffc36363c463e3f56307c469d7226a425e1f539
saimanasamuppalaneni <saimanasamuppalaneni@gmail.com>
2025-07-09 19:53:55 +0530

    patterns

---
+++ class 11 patterns/dimond.py
@@ -0,0 +1,18 @@
+# diamond
+n=int(input("enter the number of rows"))
+space=40
+for i in range (1,n+1,2):
+    for k in range (space):
+        print(" ",end="")
+    for j in range (1,i+1):
+        print(j,end="")
+    print()
+    space-=1
+space+=2
+for i in range (n-2,0,-2):
+    for k in range (space):
+        print(" ",end="")
+    for j in range (1,i+1):
+        print(j,end="")
+    print()
+    space+=1
