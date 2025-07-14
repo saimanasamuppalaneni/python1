97427c1750ac371bafeed4fc658a4b7225dbecda
saimanasamuppalaneni <saimanasamuppalaneni@gmail.com>
2025-07-14 18:02:49 +0530

    turtle

--- class 12 turtle/polygon.py
+++ class 12 turtle/polygon.py
@@ -1,18 +1,19 @@
+# polygon
 import turtle
 win=turtle.Screen()
 win.screensize(400,400)
 win.title("polygon")
-win.bgcolor('hotpink')
+win.bgcolor('lightcoral')
 t=turtle.Turtle()
-t.color("black")
+t.color("aquamarine")
 t.width(5)
-t.shape('turtle')
 n=6
 a=60
-t.fillcolor("green")
+t.fillcolor("aqua")
 t.begin_fill()
 for i in range(n):
     t.forward(100)
     t.left(a)
 t.end_fill()
 turtle.done()
+
