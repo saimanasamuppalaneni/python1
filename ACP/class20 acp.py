6e5cc116e9fa68981da70cd2c227cd57cdc0f76a
saimanasamuppalaneni <saimanasamuppalaneni@gmail.com>
2025-08-09 19:42:54 +0530

    acp

---
+++ ACP/class20 acp.py
@@ -0,0 +1,9 @@
+# tuple product
+nums = (2,4,8,8,3,2,9)
+print("\nOriginal tuple:  ")
+print(nums)
+print("product - multiplying all the numbers of the said tuple: ")
+m = 1
+for i in nums:
+    m *= i
+print(m)
