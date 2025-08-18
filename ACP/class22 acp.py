a8a92708274a6d676b7be7309d9a42f09d835238
saimanasamuppalaneni <saimanasamuppalaneni@gmail.com>
2025-08-18 17:25:50 +0530

    acp

---
+++ ACP/class22 acp.py
@@ -0,0 +1,5 @@
+# set symmetric difference
+set1 = {'blue','red','scarlett','magenta'}
+set2 = {'red','blue','teal','perssian'}
+print("set symmetric difference")
+print(set1.symmetric_difference(set2))
