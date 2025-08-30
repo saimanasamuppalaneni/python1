e05d4e202e14a9f7dce02bacb4550a0524a81e05
saimanasamuppalaneni <saimanasamuppalaneni@gmail.com>
2025-08-30 16:09:17 +0530

    acp

---
+++ ACP/class23 acp.py
@@ -0,0 +1,7 @@
+# list comprehension for odd numbers
+list1 = [1,3,12,34,45,67,98,10,37]
+listc = [i for i in list1 if i % 2 == 1]
+print(listc)
+fruits = ['apple' ,'orange','bannana','pineapple']
+list_s = [i.capitalize()for i in fruits]
+print(list_s)
